0d779b14c9c209ed5baed3f07dcb551e6cbbee24
DrEden33773 <edwardwang33773@gmail.com>
2022-10-03 21:14:14 +0800

    add test file

---
+++ tests/Matrix.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file Matrix.cpp
+ * @author Eden (edwardwang33773@gmail.com)
+ * @brief Test of the function of `Matrix` module
+ * @version 0.1
+ * @date 2022-10-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "../tools/Matrix.hpp"
+
+using Tool::Matrix;
+
+int main(int argc, char** argv) {
+    Matrix<>::example();
+}
