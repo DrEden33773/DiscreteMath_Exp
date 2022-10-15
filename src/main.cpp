211e8b2855724a2d1d3af101181a8c7837d2ba7c
DrEden33773 <edwardwang33773@gmail.com>
2022-10-15 23:17:58 +0800

    better format

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,11 @@
 
 int main(int argc, char** argv) {
     // Tests below could be recalled!
-    //
+
     // Test::MatrixTest();
     // Test::UndirectedGraphTest();
     // Test::EulerTest_undirected();
     // Test::EulerTest_directed();
-    //
 
     Graph the_graph = GraphFactory::CreateGraph();
     the_graph.show_euler_circle_set_H();
