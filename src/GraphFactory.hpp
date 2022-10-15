52bcb49c14e16919f824712ce668a493a64ec0a6
DrEden33773 <96013437+DrEden33773@users.noreply.github.com>
2022-10-15 22:29:20 +0800

    finished, with bug

--- src/GraphFactory.hpp
+++ src/GraphFactory.hpp
@@ -9,9 +9,10 @@
                     @e could_be_designated_as_directed_graph
                     @e will_be_declared_as_undirected_graph
             @b RULE_OF_JUDGE:
+                @b if the @e Matrix is @b Non_Symmetrical => @p Restricted_as @e directed
+                @b if the @e Matrix is @b {{0}} => @p Restricted_as @e Undirected
                 @b if the @e Matrix is @b Axially_Symmetrical => @p Judged_as @e Undirected
-                @b if the @e Matrix is @b Non_Symmetrical => @p Judged_as @e directed
-                @b if you've @p designated the @e type => @p Abandon_Judgement
+                    @b if you've @p designated the @e type => @p Abandon_Judgement
 
  * @version 0.1
  * @date 2022-10-15
@@ -25,16 +26,211 @@
 #include "../tools/directed_graph.hpp"
 #include "../tools/undirected_graph.hpp"
 
-class GraphFactory {
-    enum class graph_type : unsigned short {
+class Graph;
+class GraphFactory;
+
+class Graph {
+    friend class GraphFactory;
+
+    enum class Type : unsigned short {
         undirected = 0,
         directed   = 1,
     };
-    GraphFactory() = default;
+    directed_graph*   directed   = nullptr;
+    undirected_graph* undirected = nullptr;
+    Type              graph_type = Type::directed; // default as directed
+
+    Graph(std::vector<std::vector<int>>&& initMat, Type type_of_graph) {
+        using origin = std::vector<std::vector<int>>;
+        graph_type   = type_of_graph;
+        if (graph_type == Type::undirected) {
+            undirected = new undirected_graph(
+                std::forward<origin>(initMat)
+            );
+        } else {
+            directed = new directed_graph(
+                std::forward<origin>(initMat)
+            );
+        }
+    }
 
 public:
-    ~GraphFactory() = default;
+    std::vector<std::string>
+    return_euler_circle_set_H() {
+        if (directed == nullptr && undirected == nullptr) {
+            std::cout << std::endl;
+            throw std::runtime_error("Graph is not initialized. ");
+        }
 
-    static GraphFactory CreateGraphInterface() {
+        std::vector<std::string> res;
+
+        if (graph_type == Type::undirected) {
+            res = directed_graph::return_euler_circle_set_H(*directed);
+        } else {
+            res = undirected_graph::return_euler_circle_set_H(*undirected);
+        }
+
+        return res;
+    }
+
+    std::vector<std::string>
+    return_euler_circle_set_F() {
+        if (directed == nullptr && undirected == nullptr) {
+            std::cout << std::endl;
+            throw std::runtime_error("Graph is not initialized. ");
+        }
+
+        std::vector<std::string> res;
+
+        if (graph_type == Type::undirected) {
+            res = directed_graph::return_euler_circle_set_F(*directed);
+        } else {
+            res = undirected_graph::return_euler_circle_set_F(*undirected);
+        }
+
+        return res;
+    }
+
+    void show_euler_circle_set_H() {
+        std::vector<std::string> output;
+        output = this->return_euler_circle_set_H();
+
+        std::cout << std::endl;
+        for (auto&& vec : output) {
+            for (auto&& str : vec) {
+                std::cout << str;
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    void show_euler_circle_set_F() {
+        std::vector<std::string> output;
+        output = this->return_euler_circle_set_F();
+
+        std::cout << std::endl;
+        for (auto&& vec : output) {
+            for (auto&& str : vec) {
+                std::cout << str;
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+};
+
+class GraphFactory {
+    static constexpr bool if_any_less_than_zero(Tool::Matrix<int>& inputMat) {
+        size_t row = inputMat.get_sizeof_row();
+        size_t col = inputMat.get_sizeof_col();
+        for (size_t trow = 1; trow <= row; ++trow) {
+            for (size_t tcol = 1; tcol <= row; ++tcol) {
+                if (inputMat(row, col) == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+public:
+    GraphFactory()  = delete;
+    ~GraphFactory() = default;
+    static Graph CreateGraph() {
+        int         num_of_v   = 0;
+        Graph::Type graph_type = Graph::Type::directed;
+
+        Tool::Matrix<int>*            TheMat = nullptr;
+        std::vector<std::vector<int>> initMat;
+        std::vector<int>              initRow;
+
+        while (true) {
+            std::cout << std::endl;
+            std::cout << "Input num of vertex (>=0) => ";
+
+            std::cin >> num_of_v;
+
+            if (num_of_v < 0) {
+                std::cout << std::endl;
+                std::cout << "Input num should >=0, please do it again. " << std::endl;
+            } else {
+                break;
+            }
+        }
+
+        if (num_of_v != 0) {
+            initMat.reserve(num_of_v);
+            initRow.reserve(num_of_v);
+
+            std::cout << std::endl;
+            std::cout << "Input each element in the Adjacency Matrix => " << std::endl;
+
+            for (size_t row = 0; row < num_of_v; ++row) {
+                for (size_t col = 0; col < num_of_v; ++col) {
+                    int tmp;
+                    std::cin >> tmp;
+                    initRow.emplace_back(tmp);
+                }
+                initMat.emplace_back(initRow);
+                initRow.clear();
+            }
+
+            TheMat          = new Tool::Matrix<int>(std::move(initMat));
+            auto& TheMatRef = *TheMat;
+
+            if (if_any_less_than_zero(TheMatRef)) {
+                std::cout << std::endl;
+                throw std::logic_error("There's element <0 in the Matrix. ");
+            }
+            if (!Tool::Matrix<int>::
+                    if_symmetric_of_main_diagonal(TheMatRef)) {
+                graph_type = Graph::Type::directed;
+                std::cout << std::endl;
+                std::cout << "Type of Graph is Automatically-Judged as {directed_graph} " << std::endl;
+            } else {
+                std::cout << std::endl;
+                std::cout << "You need confirm the Type of Graph. " << std::endl;
+
+                std::cout << std::endl;
+                std::cout << R"(If you want a {directed_graph}, input "Y" or "y". )"
+                          << std::endl;
+                std::cout << R"(If you want a {undirected_graph}, input anything except Y/y. )"
+                          << std::endl;
+                std::cout << std::endl;
+                std::cout << "=> ";
+
+                std::string flag;
+                std::cin >> flag;
+
+                if (flag == "Y" || flag == "y") {
+                    graph_type = Graph::Type::directed;
+                    std::cout << std::endl;
+                    std::cout << "Type confirmed as {directed}. " << std::endl;
+                } else {
+                    graph_type = Graph::Type::undirected;
+                    std::cout << std::endl;
+                    std::cout << "Type confirmed as {undirected}. " << std::endl;
+                }
+            }
+        } else {
+            graph_type = Graph::Type::undirected;
+            TheMat     = new Tool::Matrix<int>({ { 0 } });
+            initMat    = { { 0 } };
+            std::cout << std::endl;
+            std::cout << "Type of Graph is Automatically-Judged as {undirected_graph}" << std::endl;
+        }
+
+        Graph res(std::move(initMat), graph_type);
+
+        std::cout << std::endl;
+        if (graph_type == Graph::Type::undirected) {
+            std::cout << "Successfully created a {undirected} graph" << std::endl;
+        } else {
+            std::cout << "Successfully created a {directed} graph" << std::endl;
+        }
+
+        delete TheMat;
+        return res;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 #include "../tests/EulerTest_undirected.hpp"
 #include "../tests/MatrixTest.hpp"
 #include "../tests/UndirectedGraphTest.hpp"
+#include "./GraphFactory.hpp"
 
 int main(int argc, char** argv) {
+    // Tests below could be recalled!
+    //
     // Test::MatrixTest();
     // Test::UndirectedGraphTest();
-    Test::EulerTest_undirected();
-    Test::EulerTest_directed();
+    // Test::EulerTest_undirected();
+    // Test::EulerTest_directed();
+    //
+
+    Graph the_graph = GraphFactory::CreateGraph();
+    the_graph.show_euler_circle_set_H();
+    the_graph.show_euler_circle_set_F();
 }
